1a5a1dc38e034f72ccb1e22c2079d3128e0f41ec
paideur <toussaintpahima@gmail.com>
2016-12-12 23:11:35 +0100

    verification de la variable session
    Verification de la variable session dans les pages necessitant la
    connexion de l'utilisateur

--- artisteAdmin/editerprofil.php
+++ artisteAdmin/editerprofil.php
@@ -8,39 +8,43 @@
 require('../Includes/db.php');
 session_start();
 
-//Selection de l'artiste
-$idUser=$_SESSION['id_user'];
+ if(isset($_SESSION['id_user'])){
+     //Selection de l'artiste
+    $idUser=$_SESSION['id_user'];
 
-//Traitement de la modification
-if(!empty($_POST)) {
-    
-    //Creation de la date de naissance
-    $jour=$_POST['dateNaissJour'];
-    $mois=$_POST['dateNaissMois'];
-    $annee=$_POST['dateNaissAnnee'];
-    $dateNaiss = $annee.'-'.$mois.'-'.$jour;
-    
-    //echo $_POST['prenom'];
-    // On modifie les informations dans la base de données
-    $req = $cnx->prepare("UPDATE t_users set UPDATE_DATE=:updateDate,GENDER=:genderArtist, LAST_NAME=:nomArtist,FIRST_NAME=:prenomArtist,BORN_DATE=:dateNaissArtist,ADDRESS=:adressArtist,ZIP_CODE=:codePostalArtist,
-        CITY=:villeArtist,TEL=:telArtist,LANGUAGE=:langueArtist,BLOG_NAME=:blogNameArtist,PROF_ARTISTIQUE=:profArtist,COUNTRY=:countryArtist where ID_USER=:idArtist");
-    $req->execute([
-        ':updateDate' => date('Y-m-d H:i:s'),
-        ':genderArtist' => $_POST['gender'],
-        ':nomArtist' => $_POST['nom'],
-        ':prenomArtist' => $_POST['prenom'],
-        ':dateNaissArtist' => $dateNaiss,
-        ':adressArtist' => $_POST['adresse'],
-        ':codePostalArtist' => $_POST['codePostal'],
-        ':villeArtist' => $_POST['ville'],
-        ':telArtist' => $_POST['tel'],
-        ':langueArtist' => $_POST['langue'],
-        ':blogNameArtist' => $_POST['blog_name'],
-        ':profArtist' => $_POST['prof_artistique'],
-        ':countryArtist' => $_POST['country'],
-        ':idArtist' => $idUser
-    ]);
+    //Traitement de la modification
+    if(!empty($_POST)) {
+        
+        //Creation de la date de naissance
+        $jour=$_POST['dateNaissJour'];
+        $mois=$_POST['dateNaissMois'];
+        $annee=$_POST['dateNaissAnnee'];
+        $dateNaiss = $annee.'-'.$mois.'-'.$jour;
+        
+        //echo $_POST['prenom'];
+        // On modifie les informations dans la base de données
+        $req = $cnx->prepare("UPDATE t_users set UPDATE_DATE=:updateDate,GENDER=:genderArtist, LAST_NAME=:nomArtist,FIRST_NAME=:prenomArtist,BORN_DATE=:dateNaissArtist,ADDRESS=:adressArtist,ZIP_CODE=:codePostalArtist,
+            CITY=:villeArtist,TEL=:telArtist,LANGUAGE=:langueArtist,BLOG_NAME=:blogNameArtist,PROF_ARTISTIQUE=:profArtist,COUNTRY=:countryArtist where ID_USER=:idArtist");
+        $req->execute([
+            ':updateDate' => date('Y-m-d H:i:s'),
+            ':genderArtist' => $_POST['gender'],
+            ':nomArtist' => $_POST['nom'],
+            ':prenomArtist' => $_POST['prenom'],
+            ':dateNaissArtist' => $dateNaiss,
+            ':adressArtist' => $_POST['adresse'],
+            ':codePostalArtist' => $_POST['codePostal'],
+            ':villeArtist' => $_POST['ville'],
+            ':telArtist' => $_POST['tel'],
+            ':langueArtist' => $_POST['langue'],
+            ':blogNameArtist' => $_POST['blog_name'],
+            ':profArtist' => $_POST['prof_artistique'],
+            ':countryArtist' => $_POST['country'],
+            ':idArtist' => $idUser
+        ]);
 
-}
+    }
+ }
+ else echo "<script type='text/javascript'>document.location.replace('connexion.html');</script>";
+
 
 ?>
--- artisteAdmin/editerprofil_connexion.php
+++ artisteAdmin/editerprofil_connexion.php
@@ -8,21 +8,23 @@
 require('../Includes/db.php');
 session_start();
 
-//Selection de l'artiste
-$idUser=$_SESSION['id_user'];
+ if(isset($_SESSION['id_user'])){
+    //Selection de l'artiste
+    $idUser=$_SESSION['id_user'];
 
-//Traitement de la modification
-if(!empty($_POST)) {   
-    // On ne sauvegardera pas le mot de passe en clair dans la base mais plutôt un hash
-    $password = password_hash($_POST['mtpasse'], PASSWORD_BCRYPT); 
+    //Traitement de la modification
+    if(!empty($_POST)) {   
+        // On ne sauvegardera pas le mot de passe en clair dans la base mais plutôt un hash
+        $password = password_hash($_POST['mtpasse'], PASSWORD_BCRYPT); 
 
-    // On modifie les informations dans la base de données
-    $req = $cnx->prepare("UPDATE t_users set EMAIL=:emailArtist,PASSWORD=:passwordArtist where ID_USER=:idArtist");
-    $req->execute([
-        ':emailArtist' => $_POST['email'],
-        ':passwordArtist' => $password,
-        ':idArtist' => $idUser
-    ]);
-}
+        // On modifie les informations dans la base de données
+        $req = $cnx->prepare("UPDATE t_users set EMAIL=:emailArtist,PASSWORD=:passwordArtist where ID_USER=:idArtist");
+        $req->execute([
+            ':emailArtist' => $_POST['email'],
+            ':passwordArtist' => $password,
+            ':idArtist' => $idUser
+        ]);
+    }
+ }else echo "<script type='text/javascript'>document.location.replace('connexion.html');</script>";
 
 ?>
